210b99f9faafe2eba5fa4f2dfc43e0dc990a0004
e-tat-98 <egutan1016@gmail.com>
2026-03-01 01:15:24 +0900

    :wrench: Add base path

---  vite.config.js
+++  vite.config.js
@@ -1,19 +1,20 @@
 import svgLoader from 'vite-svg-loader'
 
 export default {
+  base: '/my-slides/',
   plugins: [
     svgLoader(),
-    {
-      name: 'replace-presenter-image-path',
-      apply: 'build',
-      transform(code, id) {
-        if (!id.includes('slides.md')) return
+    // {
+    //   name: 'replace-presenter-image-path',
+    //   apply: 'build',
+    //   transform(code, id) {
+    //     if (!id.includes('slides.md')) return
 
-        return code.replace(
-          /presenterImage:\s*['"]\/my-pic\.jpg['"]/g,
-          "presenterImage: '/my-slides/my-pic.jpg'"
-        )
-      }
-    }
+    //     return code.replace(
+    //       /presenterImage:\s*['"]\/my-pic\.jpg['"]/g,
+    //       "presenterImage: '/my-slides/my-pic.jpg'"
+    //     )
+    //   }
+    // }
   ],
 }
