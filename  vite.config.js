c9fc383a37c7b77eaa902f0664f5518e3d191d69
e-tat-98 <egutan1016@gmail.com>
2026-03-01 01:12:54 +0900

    :adhesive_bandage: Change src path in pruduction

---  vite.config.js
+++  vite.config.js
@@ -1,5 +1,19 @@
 import svgLoader from 'vite-svg-loader'
 
 export default {
-  plugins: [svgLoader()],
+  plugins: [
+    svgLoader(),
+    {
+      name: 'replace-presenter-image-path',
+      apply: 'build',
+      transform(code, id) {
+        if (!id.includes('slides.md')) return
+
+        return code.replace(
+          /presenterImage:\s*['"]\/my-pic\.jpg['"]/g,
+          "presenterImage: '/my-slides/my-pic.jpg'"
+        )
+      }
+    }
+  ],
 }
